f07c52f06da47eac27c61624ec2a668c0fcee432
Dima Gudkov <d.o.gudkov@gmail.com>
2020-04-08 16:20:09 +0300

    sensor type

---
+++ database/seeds/SensorTypeSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SensorTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
