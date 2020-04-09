d2e41ac9d7d0878c46bf6a0bde327fb5c53a6649
Dima Gudkov <d.o.gudkov@gmail.com>
2020-04-10 01:06:14 +0300

    edit seeder

--- database/seeds/SensorTypeSeeder.php
+++ database/seeds/SensorTypeSeeder.php
@@ -13,14 +13,14 @@ class SensorTypeSeeder extends Seeder
     public function run()
     {
         DB::table('sensor_type')->insert([
-            'title' => "Basic",
-            'text' => "<p>A small Welasensor the size of a matchbox, which can be connected anywhere in the house where there is a charger, and it is also compatible with work from powerbank.</p><p>Get temperature, humidity,  pressure, luminosity, noise level</p><p>You can choose corpuse material from plastic or wood</p><p>Free cloud web application</p>",
+            'name' => "Basic",
+            'description' => "<p>A small Welasensor the size of a matchbox, which can be connected anywhere in the house where there is a charger, and it is also compatible with work from powerbank.</p><p>Get temperature, humidity,  pressure, luminosity, noise level</p><p>You can choose corpuse material from plastic or wood</p><p>Free cloud web application</p>",
             'price' => 39,
             'isBig' => 1,
         ]);
         DB::table('sensor_type')->insert([
-            'title' => "CO2",
-            'text' => "<p class='red'>All in basic</p> <p>Get CO2 level</p> <p>You can choose corpus material from plastic, wood and waterproof metal for outside use</p>",
+            'name' => "CO2",
+            'description' => "<p class='red'>All in basic</p> <p>Get CO2 level</p> <p>You can choose corpus material from plastic, wood and waterproof metal for outside use</p>",
             'price' => 49,
             'isBig' => 0,
         ]);
