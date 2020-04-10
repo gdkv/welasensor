5cb6446c3ac37fcca0d92333e064f0a38ccecabd
Dima Gudkov <d.o.gudkov@gmail.com>
2020-04-11 01:42:20 +0300

    styles for verify email

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -2,17 +2,16 @@
 @section('content')
     @include('partials.title', [
         'header' => 'Verify your email address',
-        'description' => 'A fresh verification link has been sent to your email address.',
+        'description' => 'A fresh verification link has been sent to your email address. Before proceeding, please check your email for a verification link. If you did not receive the email press button below.',
         'showSensor' => true
     ])
-    {{ __('Before proceeding, please check your email for a verification link.') }}
-    {{ __('If you did not receive the email') }},
+
     <div class="form-wrapper">
         <div class="form form-aside">
             <div class="form-content">
                 <form method="POST" action="{{ route('verification.resend') }}">
                     @csrf
-                    <button type="submit">Click here to request another</button>
+                    <button class="btn btn-left" type="submit">Click here to request another</button>
                 </form>
             </div>
         </div>
